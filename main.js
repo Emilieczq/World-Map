a3e01276fb0607ca3ecdf2cc67949c1f29fd4ce4
Zhenqi <chaizhenqi@outlook.com>
2020-02-22 20:13:47 +0800

    equirectangular projection, separate css and js

---
+++ main.js
@@ -0,0 +1,18 @@
+var width = 960,
+    height = 490;
+
+var svg = d3.select("body").append("svg")
+.attr("width", width)
+.attr("height", height);
+
+d3.json("world.json", function(error, world) {
+    if (error) return console.error(error);
+    console.log(world);
+
+    svg.selectAll(".country")
+    .data(topojson.feature(world, world.objects.countries).features)
+    .enter().append("path")
+    .attr("class", function(d) { return "country " + d.id; })
+    .attr("d", d3.geo.path().projection(d3.geo.equirectangular()));
+    
+});
